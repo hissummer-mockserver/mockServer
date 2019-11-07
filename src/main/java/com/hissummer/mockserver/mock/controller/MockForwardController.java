e05b8004dda13febb8b48e297d6fc3bfac726a0e
lihao <nighteblis@hotmail.com>
2019-11-08 00:11:12 +0800

    forward controller return responseEntity

--- src/main/java/com/hissummer/mockserver/mock/controller/MockForwardController.java
+++ src/main/java/com/hissummer/mockserver/mock/controller/MockForwardController.java
@@ -11,8 +11,13 @@ import org.apache.catalina.connector.Response;
 import org.apache.catalina.connector.ResponseFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -47,9 +52,9 @@ public class MockForwardController implements ErrorController {
 	 * @param response
 	 * @return
 	 */
-	@RequestMapping(value = "/forward", produces = { "application/json" })
-	public Object forward(HttpServletRequest request, @RequestHeader Map<String, String> headers,
-			@Nullable @RequestBody String requestBody, HttpServletResponse response) {
+	@RequestMapping(value = "/forward")
+	public ResponseEntity<?> forward(HttpServletRequest request, @RequestHeader Map<String, String> headers,
+			@Nullable @RequestBody String requestBody, final HttpServletResponse response) {
 
 		log.info("headers: {}", JSON.toJSONString(headers));
 
@@ -65,20 +70,20 @@ public class MockForwardController implements ErrorController {
 			if (statusCode == HttpStatus.NOT_FOUND.value()) {
 
 				// 404 errors
-				ResponseFacade responsefacade = (ResponseFacade) response;
+//				ResponseFacade responsefacade = (ResponseFacade) response;
 
 				try {
 
 					// Here use reflect to modify return http status code 404 to 200.
 					// More details please linke into
 					// https://blog.hissummer.com/2019/07/%E5%88%A9%E7%94%A8springboot%E5%AE%9E%E7%8E%B0http-mock-%E6%9C%8D%E5%8A%A1/
-					Field innerResponse = getField(responsefacade.getClass(), "response");
-					innerResponse.setAccessible(true);
-					Response innterResponseObject = (Response) innerResponse.get(responsefacade);
-					org.apache.coyote.Response coyoteResponse = innterResponseObject.getCoyoteResponse();
-					Field httpstatus = getField(coyoteResponse.getClass(), "status");
-					httpstatus.setAccessible(true);
-					httpstatus.set(coyoteResponse, 200);
+//					Field innerResponse = getField(responsefacade.getClass(), "response");
+//					innerResponse.setAccessible(true);
+//					Response innterResponseObject = (Response) innerResponse.get(responsefacade);
+//					org.apache.coyote.Response coyoteResponse = innterResponseObject.getCoyoteResponse();
+//					Field httpstatus = getField(coyoteResponse.getClass(), "status");
+//					httpstatus.setAccessible(true);
+//					httpstatus.set(coyoteResponse, 200);
 
 //					Field httpContentType = getField(coyoteResponse.getClass(), "contentType");
 //					httpContentType.setAccessible(true);
@@ -86,27 +91,38 @@ public class MockForwardController implements ErrorController {
 
 					// This code should not throw exception.
 
-				} catch (NoSuchFieldException e) {
-					log.info(e.getMessage());
+//				} catch (NoSuchFieldException e) {
+//					log.info(e.getMessage());
 				} catch (IllegalArgumentException e) {
 					log.info(e.getMessage());
-				} catch (IllegalAccessException e) {
-					log.info(e.getMessage());
+//				} catch (IllegalAccessException e) {
+//					log.info(e.getMessage());
 				} catch (Exception e) {
 					log.info(e.getMessage());
 				}
 
-				return mockservice.getResponse(headers, host, request.getMethod(),
-						(String) request.getAttribute(RequestDispatcher.FORWARD_REQUEST_URI), requestBody);
+				HttpHeaders responseHeaders = new HttpHeaders();
+				  responseHeaders.setContentType(new MediaType("application", "json"));
+				
+				return new ResponseEntity<String>(mockservice.getResponse(headers, host, request.getMethod(),
+						(String) request.getAttribute(RequestDispatcher.FORWARD_REQUEST_URI), requestBody), responseHeaders, HttpStatus.OK);
+						
 
 			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				
+				HttpHeaders responseHeaders = new HttpHeaders();
+				  responseHeaders.setContentType(new MediaType("application", "json"));
 				// 5xx errors
-				return MockRuleMgmtResponseVo.builder().status(0).success(false).message(errorMessage).build()
-						.toString();
+				return new ResponseEntity<>(MockRuleMgmtResponseVo.builder().status(0).success(false).message(errorMessage).build()
+						.toString(),responseHeaders,HttpStatus.INTERNAL_SERVER_ERROR);
 			}
 		}
+		
+		HttpHeaders responseHeaders = new HttpHeaders();
+		  responseHeaders.setContentType(new MediaType("application", "json"));
+		  
 		// if statusCode is not 404 or 500 errors , will return default response.
-		return MockRuleMgmtResponseVo.builder().status(0).success(false).message(errorMessage).build().toString();
+		return new ResponseEntity<>(MockRuleMgmtResponseVo.builder().status(0).success(false).message(errorMessage).build().toString(),responseHeaders,HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	/**
