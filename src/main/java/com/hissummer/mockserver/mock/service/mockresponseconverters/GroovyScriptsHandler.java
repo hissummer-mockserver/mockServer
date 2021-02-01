b817449c340eb4b697fc67546bb8b5212d297c38
lihao01 <lihao01@heika.com>
2021-02-01 17:32:00 +0800

    optimize the engine obtain

--- src/main/java/com/hissummer/mockserver/mock/service/mockresponseconverters/GroovyScriptsHandler.java
+++ src/main/java/com/hissummer/mockserver/mock/service/mockresponseconverters/GroovyScriptsHandler.java
@@ -1,11 +1,14 @@
 package com.hissummer.mockserver.mock.service.mockresponseconverters;
 
+import java.util.Date;
 import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSON;
 import com.hissummer.mockserver.mock.service.mockresponseconverters.converterinterface.ScriptsConverterInterface;
+
+import groovy.transform.CompileStatic;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -19,11 +22,14 @@ import javax.script.ScriptException;
 @Component
 public class GroovyScriptsHandler implements ScriptsConverterInterface {
 
+	
+	private static final ScriptEngine engine = new ScriptEngineManager().getEngineByName("groovy");
+	
+	
 	@Override
+	@CompileStatic
 	public String converter(String originalResponse, Map<String, String> requestHeders, String requestBody) {
 
-		ScriptEngineManager factory = new ScriptEngineManager();
-		ScriptEngine engine = factory.getEngineByName("groovy");
 		engine.put("response", originalResponse);
 		engine.put("requestBody", requestBody);
 		engine.put("requestHeaders", JSON.toJSONString(requestHeders));
@@ -35,6 +41,27 @@ public class GroovyScriptsHandler implements ScriptsConverterInterface {
 		}
 
 		return engine.get("response").toString();
+	}
+	
+	//@CompileStatic
+	public static void main (String[] args)
+	{
+		System.out.println( (new Date()).getTime());
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		System.out.println( (new Date()).getTime());
+		ScriptEngine jsEngine = mgr.getEngineByName("groovy");
+		System.out.println( (new Date()).getTime());
+		  try {
+			  System.out.println( (new Date()).getTime());
+			  jsEngine.put("response", "hi");
+			  System.out.println( (new Date()).getTime());
+			  jsEngine.eval("response='hello'");
+			  System.out.println( (new Date()).getTime());
+		    
+		  } catch (ScriptException ex) {
+		      ex.printStackTrace();
+		  }
+		  System.out.println(jsEngine.get("response").toString());
 	}
 
 }
